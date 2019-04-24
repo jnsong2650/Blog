120b95a917d7c1f4b4f89b3b4b3ef4ded9b538d1
songjianan <song_121292057@126.com>
2019-04-24 16:30:45 +0800

    'update'

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -5,6 +5,16 @@ namespace App\Http\Controllers\Wechat;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+define("TOKEN", "blogWechat");    //定义TOKEN, “peng”是自己随便定义，这一句很重要！！！
+$wechatObj = new WechatController();
+
+if (!isset($_GET['echostr'])) {
+    $wechatObj->LogicAction();    //后续的有实质功能的function(此篇不用管）
+}else{
+    $wechatObj->valid();    //调用valid函数进行基本配置
+}
+
+
 class WechatController extends Controller
 {
     private $access_token;    //定义一个access_token，用于后续调用微信接口（此篇用不到）
@@ -13,13 +23,8 @@ class WechatController extends Controller
     public function wechatDefault()
     {
 
-        define("TOKEN", "blogWechat");    //定义TOKEN, “peng”是自己随便定义，这一句很重要！！！
-        $wechatObj = new wechatCallbackapiTest();
-        if (!isset($_GET['echostr'])) {
-            $wechatObj->LogicAction();    //后续的有实质功能的function(此篇不用管）
-        }else{
-            $wechatObj->valid();    //调用valid函数进行基本配置
-        }
+
+
 
     }
 
