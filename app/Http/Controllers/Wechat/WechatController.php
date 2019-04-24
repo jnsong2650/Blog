63ab9c6d58dc64973ce4bf2b39d052e0b05d0f90
songjianan <song_121292057@126.com>
2019-04-24 15:04:38 +0800

    'fds'

---
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Wechat;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WechatController extends Controller
+{
+    //微信服务器地址
+    public function wechatDefault()
+    {
+        echo 'date';
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('wechat', 'Wechat/WechatController@wechatDefault');
